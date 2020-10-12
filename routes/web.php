c4b54a93b493d7e07775ac8ba2e14d1c253526d4
Solaiman Hossain <32815625+Solaiman0168@users.noreply.github.com>
2020-10-12 09:02:54 +0600

    student post

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,9 @@ Route::get('/create-subject', 'SubjectController@create')->name('create-subject'
 Route::get('/department-index', 'DepartmentController@index')->name('department-index');
 Route::get('/create-department', 'DepartmentController@create')->name('create-department');
 
+Route::get('/post/index', 'PostController@index')->name('post.index');
+Route::get('/create/post', 'PostController@create')->name('create.post');
+
 
 Route::post('/store', 'StudentController@store')->name('store');
 //Route::post('/store', 'StudentController@store');
